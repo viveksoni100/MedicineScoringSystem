4833e5e6efc7235238d013f0f7243611304851c7
root <root@localhost.localdomain>
2019-06-05 14:29:06 +0530

    loading form's background is set

--- src/medicinescoringsystem/MedicineScoringSystem.java
+++ src/medicinescoringsystem/MedicineScoringSystem.java
@@ -17,11 +17,7 @@ public class MedicineScoringSystem {
     public static void main(String[] args) {
         // TODO code application logic here
 
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new LoadingForm().setVisible(true);
-            }
-        });
+                new LoadingForm();
 
     }//mainEnds
 
